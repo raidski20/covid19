5bc880f0f491e695ec1bbb72eb2b961f91ae8fb8
raid21 <raidski20@gmail.com>
2020-03-27 13:27:11 +0100

    optimizing the charts code on js file

--- js/main.js
+++ js/main.js
@@ -15,70 +15,55 @@ $(function () {
 
 // chart colors
 var colors = ['#043353', '#bc2c3d'];
+var days = ["Jan 20", "Jan 28", "Feb 03", "Feb 09", "Feb 15", "Feb 22", "Feb 27", "Mar 04", "Mar 10", " Mar 16", "Mar 22", "Mar 25"];
+var chart_ids = ["chLine", "chLinee"];
 
-/* line chart 1 */
-var chLine = document.getElementById("chLine");
-var chartData = {
-    labels: ["Jan 20", "Jan 28", "Feb 03", "Feb 09", "Feb 15", "Feb 22", "Feb 27", "Mar 04", "Mar 10", " Mar 16", "Mar 22", "Mar 25"],
-    datasets: [{
-        data: [282, 4593, 17391, 40000, 67000, 76288, 82000, 95000, 118000, 167511, 335000, 413433],
-
-        backgroundColor: 'transparent',
-        borderColor: colors[0],
-        borderWidth: 4,
-        pointBackgroundColor: colors[0]
-    }]
-};
-if (chLine) {
-    new Chart(chLine, {
-        type: 'line',
-        data: chartData,
-        options: {
-            scales: {
-                xAxes: [{
-                    ticks: {
-                        beginAtZero: false
-                    }
+for (var i = 0; i < chart_ids.length; i++) 
+{
+    var chLine = document.getElementById(chart_ids[i]);
+    switch (i) {
+        case 0:
+            var chartData = {
+                labels: days,
+                datasets: 
+                [{
+                    data: [282, 4593, 17391, 40000, 67000, 76288, 82000, 95000, 118000, 167511, 335000, 413433],
+                    borderColor: colors[i],
+                    borderWidth: 4,
                 }]
-            },
-            legend: {
-                display: false
-            },
-            responsive: true
-        }
-    });
-}
+            };
+        break;
 
-// line chart 2
-chLine = document.getElementById("chLinee");
-chartData = {
-    labels: ["Jan 20", "Jan 28", "Feb 03", "Feb 09", "Feb 15", "Feb 22", "Feb 27", "Mar 04", "Mar 10", " Mar 16", "Mar 22", "Mar 25"],
-    datasets: [
-        {
-            data: [6, 362, 813, 1526, 2359, 2804, 3198, 4012, 6606, 12783, 18433],
-            backgroundColor: colors[3],
-            borderColor: colors[1],
-            borderWidth: 4,
-            pointBackgroundColor: colors[1]
-        }]
-};
-if (chLine) {
-    new Chart(chLine, {
-        type: 'line',
-        data: chartData,
-        options: {
-            scales: {
-                xAxes: [{
-                    ticks: {
-                        beginAtZero: false
-                    }
+        case 1:
+            chartData = {
+                labels: days,
+                datasets: 
+                [{
+                    data: [6, 362, 813, 1526, 2359, 2804, 3198, 4012, 6606, 12783, 18433],
+                    borderColor: colors[i],
+                    borderWidth: 4,
                 }]
-            },
-            legend: {
-                display: false
-            },
-            responsive: true
-        }
-    });
-}
+            };
+        break;
+    }
 
+    if (chLine) {
+        new Chart(chLine, {
+            type: 'line',
+            data: chartData,
+            options: {
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero: false
+                        }
+                    }]
+                },
+                legend: {
+                    display: false
+                },
+                responsive: true
+            }
+        });
+    }
+}
